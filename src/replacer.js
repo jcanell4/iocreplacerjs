0381b5c49a9565e365259ef3d588ec160a2305d5
jcanell4 <jcanell4@xtec.cat>
2022-12-02 12:15:01 +0100

    canvis documentació i canvis en el reemplaçament d'atributs

--- src/replacer.js
+++ src/replacer.js
@@ -13,15 +13,17 @@
  *              bé ".classeX" o qualsevol altre selecctor CSS vàlid. Pot ser opcional si només es fa servir com a valor 
  *              de referència d'altres comande de reemplaçament.
  *   - type: indica el tipus de remplaçament. Pot prendre els següent valors: text, html, atrr, arrayText, ref, file/txt i file/html. 
- *              EL valor text reemplacaçar+a a totes les etiquetes HTML de la pàgina o fer la substitució, seleccionades amb el selector, amb el text contingut al paràmetre 'value'.
- *              EL valor html reemplacaçar+a a totes les etiquetes HTML de la pàgina o fer la substitució, seleccionades amb el selector, el seu contingut amb el valor html dal paràmetre 'value'.
- *              El valor attr, reemplaçarà l'atribut anomenat com el valor de la propietat 'name' amb el valor de la propietat 'value'
+ *              EL valor text inserirà/reemplaçarà a totes les etiquetes HTML seleccionades amb el selector, de la pàgina on s'executi, el text contingut al paràmetre 'value'.
+ *              EL valor html inserirà/reemplaçarà a totes les etiquetes HTML de la pàgina on s'executi, seleccionades amb el selector, el contingut html dal paràmetre 'value'.
+ *              El valor attr, reemplaçarà l'atribut anomenat com el valor de la propietat 'name' amb el valor de la propietat 'value'. Si no conté la propietat 'if_exist', s'inserirà 
+ *              l'atribut tan si exsiteix préviament com si no. si la conté, en cas que el valor sigui 'true' només es farà la substitució en el cas que l'atribut de l'etiqueta html 
+ *              existreixi. Si no exsiteix no es farà la sustitució. En cas que la propiatat if_exist no valgui 'true', es comportarà igual que si no existiś aquesta propietat.
  *              EL valor arrayText permet concatenar un conjunt de valors de tipus arrayjson ([..., ...]). fent servir un caracter o conjunt de caràctes separadors. 
- *              EL valor ref, permet fer referpencia a una comanda de reemplaçament definida enb una altre lloc (diferent identificador), però en el mateix fitxer json, EL paràmetre 'name' indicarà 
- *              el nom de l'identificador on es troba la instrucció de reemplaçament i la propietat 'value' serà la key usada per extreure la comanda específica.
- *              El valor file/text i file/html tene la mateixa funció que text o html, però amb la diferenci que el valor, en compte de ser el contingut de reemplaçament, és una URL apuntant a un fitxer extern 
- *              de text on es trova el text o htm a substituir.
- *              La propietat tye és opcional perquè en el seu lloc podem fer servir la propietat defaultValues, el valor de la qual indica la key de l'identificador defaultValues que caldrà 
+ *              EL valor ref, permet fer referpencia a una comanda de reemplaçament definida en una altre lloc (diferent identificador), però en el mateix fitxer de remmplaçament json, 
+ *              El paràmetre 'name' indicarà el nom de l'identificador on es troba la instrucció de reemplaçament i la propietat 'value' serà la key usada per extreure la comanda específica.
+ *              Els valors file/text i file/html tenen la mateixa funció que text o html, però amb la diferència que els valors, en comptes de contenir el valor del reemplaçament, sóns una URL 
+ *              apuntant a un fitxer extern de text on es trova el text o htm a substituir.
+ *              La propietat type és opcional perquè en el seu lloc podem fer servir la propietat defaultValues, el valor de la qual indica la key de l'identificador defaultValues que caldrà 
  *              fer servir per obtenir la instrucció de reeemplaçament.
  *   - value: segons el type indica una o altre cosa, però en general indica el valor que caldrà reemplaçar
  *   - name: segons el type indica diferents coses. Per exemple si el type és attr, name indica el nom de l'atribut, però si es ref, name inidcarà el valor de l'identificador on trobarem la 
@@ -45,9 +47,82 @@
  *                                       o síncrona (false) i la funció de control d'error en cas que no s'aconsegueixi carregar el json.                                      s'aconsegueixi carregar el json.
  * 
  * La funció createReplacerFromTagConfig, permet la inicialització automàtica a partir d'una etiqueta HTM anomenada replacer-config, la qual contindrà els següents atributs:
- * data-url: indica la URL on es troba el json amb les dades a substituir. Exemple: "https://ioc.xtec.cat/materials/FP/json_pt_data/replacedata/documents_fp_plans_de_treball_pt_asx_m03b2.json"
- * data-id: indica la clau que contindrà les dades de substitució per aquesta pàgina. Per exemple, "M03_U4"
- * data-async: que indioca si la càrrega cal fer-la asincrona o síncrona. Es tracta d'un atrubut que admet només dos valors, true o false.
+ *   - data-url: indica la URL on es troba el json amb les dades a substituir. Exemple: "https://ioc.xtec.cat/materials/FP/json_pt_data/replacedata/documents_fp_plans_de_treball_pt_asx_m03b2.json"
+ *   - data-id: indica la clau que contindrà les dades de substitució per aquesta pàgina. Per exemple, "M03_U4"
+ *   - data-async: que indioca si la càrrega cal fer-la asincrona o síncrona. Es tracta d'un atrubut que admet només dos valors, true o false.
+ *
+ *Exemple d'HTML a reemplaçar amb etiqueta de configuració:
+ <!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; object-src 'none'; script-src resource: chrome:; connect-src https:; img-src https: data: blob: chrome:; style-src 'unsafe-inline';">
+    <meta name="color-scheme" content="light dark">
+    <title data-l10n-id="newtab-page-title"></title>
+    <link rel="icon" type="image/png" href="chrome://branding/content/icon32.png"/>
+    <link rel="stylesheet" href="chrome://browser/content/contentSearchUI.css" />
+    <link rel="stylesheet" href="chrome://activity-stream/content/css/activity-stream.css" />
+  </head>
+  <body class="body1">
+    <div id="LL1">
+         <div id="LL1_SD1">
+            <p> Realitzeu aquestes activitats entre les dates: <span id="ll1sd1dataIn">ERROR dd/mm/aaaa</span>-<span id="ll1sd1dataFi">ERROR dd/mm/aaaa</span></p>
+            <div id="LL1_S1_P1">
+                <p id="ll1sd1p1_instruccions">ERROR instruccions</p>            
+                <textarea id="e1" name="e1" rows="4" cols="50">
+Escriu aquí.
+                </textarea>
+                <div id="ll1sd1p1_solucio" class"solucio">
+                    ERROR solució
+                </div>  
+                <div id="ll1sd1p1_after">
+                    ERROR reflexió sobre l'exercici
+                </div>  
+            </div>  
+            <div id="LL1_S1_P2">
+                <div id="ll1sd1p2_dX">ERROR TITOL</div>            
+                <video controls width="250">
+                    <source id="ll1sd1p2_vx" data-id="v1" src="/error.webm" type="video/webm">
+                    El teu navegador no permet veure aquest vídeo. Descarrega'l a:
+                    <a data-id="v1" href="/error.webm">WEBM</a>
+                </video>           
+            </div>  
+        </div>
+        ...
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" type="text/javascript"></script>
+    <script src="https://ioc.xtec.cat/.../replacer.min.js" type="text/javascript"></script>
+    <replacer-config data-url="https://ioc.xtec.cat/.../fitxert.json" data-id="MIX_LL1" data-async="false"/>
+  </body>
+</html>
+ 
+ *
+ *
+ *Exemple de fitxer de reemplaçament json:
+ *
+ {
+    "defaultValues:{
+        "defVal1":{"type:"attr", "name"="hidden", "value"="true"},
+        "defVal1":{"type:"text", "value"="AIXÒ ÉS UN EXEMPLE"}
+    },
+    "MIX_LL1": {
+        "ll1sd1dataIn": {"selector":"#ll1sd1dataIn", "type":"text", "value":"14/09/2022"},
+        "ll1sd1dataFi": {"selector":"#ll1sd1dataFi", "type":"ref", "name":"MIX_LL2","value":"ll1sd2dataIn"},
+        "ll1sd1p1_before": {"selector":"#ll1sd1p1_instruccions", "type":"file/text", "value":"https://ioc.xtec.cat/materials/FP/..,/text_pla.txt"},
+        "ll1sd1p1_after": {"selector":"#ll1sd1p1_before", "type":"file/html", "value":"https://ioc.xtec.cat/materials/FP/..,/text_html.html"},
+        "ll1sd1p1_solucio": {"selector":"#ll1sd1p1_solucio", "type":"file/html", "value":"https://ioc.xtec.cat/materials/FP/..,/solucioll1sd1p1.html"},
+        "ll1sd1p1_solucio_amaga": {"selector":".solucio", "defaultvalues":"defVal1"}
+        "ll1sd1p2_dada_X": {"selector":"#ll1sd1p2_dX", "type":"html", "value":"<h2>LES AMEBES</h2>"},
+        "ll1sd1p2_cont": {"selector":"[data-id=v1]", "type":"attr", "name":"src|href" "if_exist":true, "value":"https://ioc.xtec.cat/materials/FP/..,/video.webm"},
+        "ll1sd2dataIn": {"selector":"#ll2sd2dataIn", "type":"text", "value":"07/10/2022"},
+        "ll1sd2p1": {"selector":"#ll2sd2p1", "type":"file/html, "value":"https://ioc.xtec.cat/.../ll2sd1p1.html"},
+        ...
+    },
+    "MIX_LL2": {
+        ...
+    }
+}
+ *
  * 
  * La càrrega d'aquest codi implica l'execució de la funció createReplacerFromTagConfig
  */
@@ -252,7 +327,18 @@ $(document).ready(function () {
                        }else{
                            name = getValue(activity.name, defalutValues, data);
                        }
-                       $(selector).attr(name, value);
+                       var aName = name.split("|");
+                       for (var n of aName) {
+                           if(("if_exist" in activity) && activity.if_exist){
+                               $(selector).each(function(){
+                                   if($(this).prop(name)){
+                                       $(this).attr(name, value);
+                                   }
+                               });
+                           }else{
+                                $(selector).attr(name, value);
+                           }
+                       }                       
                    }           
                 };
                 var activities = data[id];    
